1aaa45f8836bd30a4e4b5d6b3b48c34e64166946
DaraghW <1015125@student.sae.edu.au>
2019-06-20 20:05:18 +0800

    Score Scripts set up
    needs testing
    canvis has 4 seperate text
    4 seperate scripts for each
    1 script for gold collision

---
+++ Greed_Is_God/Assets/Scripts/Score Scripts/PlayerOneScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerOneScore : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    Text ScoreOne;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreOne = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreOne.text = "$" + scoreValue;
+    }
+}
---
+++ Greed_Is_God/Assets/Scripts/Score Scripts/PlayerTwoScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTwoScore : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    Text ScoreTwo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreTwo = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreTwo.text = "$" + scoreValue;
+    }
+}
