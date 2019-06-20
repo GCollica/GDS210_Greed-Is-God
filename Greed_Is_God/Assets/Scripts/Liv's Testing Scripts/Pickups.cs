2127e6e6633ea96010199d26a18c80ced03265b2
oliviacarbone <49055820+oliviacarbone@users.noreply.github.com>
2019-06-21 02:12:30 +0800

    Implemented Pickup Mechanic
    - Player is able to pick up the key collectable and then successfully open the according door

---
+++ Greed_Is_God/Assets/Scripts/Liv's Testing Scripts/Pickups.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject key;
+    public Image UIKeyImage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Key"))
+        {
+            Destroy(key);
+            UIKeyImage.enabled = true;
+        }
+
+        if (UIKeyImage.enabled == true && other.CompareTag("Door"))
+        {
+            Destroy(door);
+            UIKeyImage.enabled = false;
+        }
+    }
+}
--- Greed_Is_God/Assets/Scripts/Liv's Testing Scripts/PlayerMovement.cs
+++ Greed_Is_God/Assets/Scripts/Liv's Testing Scripts/PlayerMovement.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    private float moveSpeed = 10f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
     }
 }
