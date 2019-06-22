1995ebab3243b97583c0065aaeb3766492bc42bd
GCollica <49055809+GCollica@users.noreply.github.com>
2019-06-22 12:46:46 +0800

    Created Spawner Prefab
    Also editted Room Controller Script

--- Greed_Is_God/Assets/Scripts/RoomController.cs
+++ Greed_Is_God/Assets/Scripts/RoomController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class RoomController : MonoBehaviour
 {
     public bool roomActive = false;
-    public GameObject[] spawners;
 
     // Start is called before the first frame update
     void Start()
