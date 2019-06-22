f0394827fc6384c9fa975653147572d9b86ad448
GCollica <49055809+GCollica@users.noreply.github.com>
2019-06-22 12:25:56 +0800

    Created Spawn Mechanic
    Created Spawn Mechanic, only runs when the room controller trigger has been triggered.

---
+++ Greed_Is_God/Assets/Scripts/Spawners.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawners : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject [] spawnPoints;
+    private GameObject chosenSpawnPoint;
+    private int spawnCount;
+    private float spawnDelay = 1f;
+    private float spawnTimer = 0f;
+    private RoomController roomControllerScript;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        roomControllerScript = GameObject.FindObjectOfType<RoomController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(roomControllerScript.roomActive == true)
+        {
+            SpawnTimer();
+        }      
+    }
+
+    public void Spawn()
+    {
+        if(spawnCount <= 10)
+        {
+            int spawnPointRounded;
+            spawnPointRounded = Mathf.RoundToInt(Random.Range(0f, 3f));
+
+            Instantiate(enemy, spawnPoints[spawnPointRounded].transform.position, Quaternion.identity);
+            spawnCount++;
+        }
+    }
+
+    public void SpawnTimer()
+    {
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer >= spawnDelay)
+        {
+            Spawn();
+            spawnTimer = 0f;
+        }
+    }
+}
