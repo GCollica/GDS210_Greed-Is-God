fdebb3cdc3038e55d56f107f98d5154233f6c146
DaraghW <1015125@student.sae.edu.au>
2019-06-25 20:04:17 +0800

    added enemy follow

---
+++ Greed_Is_God/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public GameObject Player;
+
+
+    public float thrust;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * thrust);
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+    }
+}
