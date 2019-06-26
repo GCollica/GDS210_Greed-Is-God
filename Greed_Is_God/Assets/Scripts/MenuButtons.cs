0cbae62c6cb809319643281da55555743df0b27e
oliviacarbone <oliviatcarbone@gmail.com>
2019-06-26 18:42:08 +0800

    Additions
    - audio manager
    - sound script
    - deletion of old scenes

---
+++ Greed_Is_God/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("AlphaTestScene");
+    }
+
+    public void ControlsButton()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
