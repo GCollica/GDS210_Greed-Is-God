de98715b6ce0a5b2bf95f4dea97d2952d74713f2
MacVP <48820988+MacVP@users.noreply.github.com>
2019-06-30 00:43:35 +0800

    Update

--- Greed_Is_God/Assets/Scripts/PlayerController.cs
+++ Greed_Is_God/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,8 @@ public class PlayerController : MonoBehaviour
 
     public bool player1;
     public bool player2;
+    public bool player3;
+    public bool player4;
 
     private PManager PM;
 
@@ -29,16 +31,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (player1 == true)
-        {
-            GetInputP1();
-        }
-
-        if (player2 == true)
-        {
-            GetInputP2();
-        }
-
+        PlayerActivate();
         OverrideRotation();
 
         //Stops the player from snapping back to 0 rotation
@@ -48,6 +41,8 @@ public class PlayerController : MonoBehaviour
         Move();   
     }
 
+    //Checks if each player is in the level
+    //There will be a menu to pick if the players corresponding player game object is in the scene before this check is called
     void JoystickCheck()
     {
         if (gameObject.name == "Player_1")
@@ -61,8 +56,45 @@ public class PlayerController : MonoBehaviour
             player2 = true;
             print("Player 2 Connected");
         }
+
+        if (gameObject.name == "Player_3")
+        {
+            player3 = true;
+            print("Player 3 Connected");
+        }
+
+        if (gameObject.name == "Player_4")
+        {
+            player4 = true;
+            print("Player 4 Connected");
+        }
     }
 
+    //Assigns each player to their coresponding joystick movement
+    void PlayerActivate()
+    {
+        if (player1 == true)
+        {
+            GetInputP1();
+        }
+
+        if (player2 == true)
+        {
+            GetInputP2();
+        }
+
+        if (player3 == true)
+        {
+            GetInputP2();
+        }
+
+        if (player4 == true)
+        {
+            GetInputP2();
+        }
+    }
+
+    //Player 1 inputs
     void GetInputP1()
     {
         //Left analogue sick movement
@@ -77,6 +109,7 @@ public class PlayerController : MonoBehaviour
             PM.Shoot();
     }
 
+    //Player 2 inputs
     void GetInputP2()
     {
         //Left analogue sick movement
@@ -91,18 +124,50 @@ public class PlayerController : MonoBehaviour
             PM.Shoot();
     }
 
+    //Player 3 inputs
+    void GetInputP3()
+    {
+        //Left analogue sick movement
+        xVel = Input.GetAxis("LeftJoystickHorizontal_2") * moveSpeed;
+        yVel = Input.GetAxis("LeftJoystickVertical_2") * moveSpeed;
+
+        //Right analogue sick movement
+        xRot = Input.GetAxisRaw("RightJoystickHorizontal_2");
+        yRot = Input.GetAxisRaw("RightJoystickVertical_2");
+
+        if (xRot != 0 || yRot != 0)
+            PM.Shoot();
+    }
+
+    //Player 4 inputs
+    void GetInputP4()
+    {
+        //Left analogue sick movement
+        xVel = Input.GetAxis("LeftJoystickHorizontal_2") * moveSpeed;
+        yVel = Input.GetAxis("LeftJoystickVertical_2") * moveSpeed;
+
+        //Right analogue sick movement
+        xRot = Input.GetAxisRaw("RightJoystickHorizontal_2");
+        yRot = Input.GetAxisRaw("RightJoystickVertical_2");
+
+        if (xRot != 0 || yRot != 0)
+            PM.Shoot();
+    }
+
+    //For aim when not moving
     void OverrideRotation()
     {
         //Stops the player from snapping back to 0 rotation
         if (xRot == 0 && (yRot) == 0)
             return;
 
-        //For aim while standing still
+        //Allows player to aim and shoot while stationary
         Vector3 lookDirection = new Vector3(xRot, 0, yRot);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), 0.5f);
         
     }
 
+    //For player aim while moving
     void Move()
     {
         rb.velocity = new Vector3(xVel , 0, yVel);
@@ -116,7 +181,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            //For aim while moving 
+            //Allows player to aim and shoot while moving
             Vector3 lookDirection = new Vector3(xRot, 0, yRot);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), 0.5f);
         }
