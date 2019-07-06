897d67e0cdf6ffab080ca2de42bf1e7c2a5ab561
MacVP <48820988+MacVP@users.noreply.github.com>
2019-07-06 23:00:26 +0800

    Updateed Player Death

--- Greed_Is_God/Assets/Scripts/PManager.cs
+++ Greed_Is_God/Assets/Scripts/PManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PManager : MonoBehaviour
 {
     public int health = 3;
+    public bool invulnerable;
+    public GameObject deathDrop;
+    private DeathDropValue deathDV;
+
     public int goldCount;
     public int keyCount;
     float nextFire = 0f;
@@ -18,6 +22,11 @@ public class PManager : MonoBehaviour
 
     private GreedGod greedGod;
 
+    private void Awake()
+    {
+        greedGod = FindObjectOfType<GreedGod>();
+    }
+
     void Update()
     {
         greedGod = FindObjectOfType<GreedGod>();
@@ -29,15 +38,24 @@ public class PManager : MonoBehaviour
 
     void Dead()
     {
-        if (health <= 0)
             gameObject.SetActive(false);
+            goldCount = goldCount / 2;
+            deathDV.deathDropValue = goldCount;
+            Instantiate(deathDrop, player.transform.position, Quaternion.identity);
             Invoke("Spawn", 3);
     }
 
     void Spawn()
     {
         gameObject.SetActive(true);
-        this.transform.position = greedGod.currentGreedGod.transform.position;
+        player.transform.position = greedGod.currentGreedGod.transform.position;
+        invulnerable = true;
+        Invoke("Invulnerability", 5);
+    }
+
+    void Invulnerability()
+    {
+        invulnerable = false;
     }
 
     public void Shoot()
@@ -49,7 +67,6 @@ public class PManager : MonoBehaviour
             nextFire = Time.time + fireRate;
             Instantiate(arrows, spawnPoint.transform.position, player.transform.rotation);          
         }
-
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -64,7 +81,7 @@ public class PManager : MonoBehaviour
             Destroy(collision.gameObject);
         }
 
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") && invulnerable == false)
         {
             health -= 1;
             Debug.Log("Ouch");
@@ -76,6 +93,11 @@ public class PManager : MonoBehaviour
         if (other.gameObject.CompareTag("Coin"))
         {
             goldCount += 10;
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("DeathDrop"))
+        {
+            goldCount += deathDV.deathDropValue;
             Destroy(other.gameObject);
         }
         if (other.gameObject.CompareTag("Key"))
