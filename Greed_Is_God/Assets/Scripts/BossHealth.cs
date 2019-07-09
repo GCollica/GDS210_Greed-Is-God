6594b1ecec71a7c2699c268df07cfafd3b2b560d
DaraghW <1015125@student.sae.edu.au>
2019-07-10 04:57:03 +0800

    can now win

--- Greed_Is_God/Assets/Scripts/BossHealth.cs
+++ Greed_Is_God/Assets/Scripts/BossHealth.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHealth : MonoBehaviour
 {
     private int health = 50;
+
+    public GameObject playerScreen;
+    public GameObject winScreen;
+    public bool isDead;
+
+    private void Start()
+    {
+        isDead = false;
+    }
 
     private void Update()
     {
         if (health <= 0)
         {
             Destroy(this.gameObject);
+            isDead = true;
+            Debug.Log("Congrats");
+        }
+
+        if (isDead == true)
+        {
+            winScreen.SetActive (true);
+            playerScreen.SetActive(false);
         }
     }
 
