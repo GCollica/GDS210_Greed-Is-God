25d1afdaca7df7c0f334386598d2ecfcad70cca1
oliviacarbone <49055820+oliviacarbone@users.noreply.github.com>
2019-07-10 03:31:27 +0800

    Changes to Level 2

--- Greed_Is_God/Assets/Scripts/BossHealth.cs
+++ Greed_Is_God/Assets/Scripts/BossHealth.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 public class BossHealth : MonoBehaviour
 {
     private int health = 50;
-    public Color red;
-    SpriteRenderer sr;
-    private Material matRed;
-    private Material matDefault;
 
     private void Update()
     {
@@ -18,19 +14,11 @@ public class BossHealth : MonoBehaviour
         }
     }
 
-    void Start()
-    {
-        sr = GetComponent<SpriteRenderer>();
-        matRed = Resources.Load("RedFlash", typeof(Material)) as Material;
-        matDefault = sr.material;
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Projectile"))
         {
             health--;
-            sr.material = matRed;
         }
     }
 }
