1fa9f918baffde1671726d17e9c4ae6f86cab64c
DaraghW <1015125@student.sae.edu.au>
2019-07-09 19:50:44 +0800

    Win room fixs

---
+++ Greed_Is_God/Assets/Scripts/WinBox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinBox : MonoBehaviour
+{
+    public bool Win = false;
+    public GameObject WinScreen;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Win = true;          
+            Debug.Log("congrats");
+        }
+    }
+    private void Update()
+    {
+        if (Win == true)
+        {
+            gameObject.SetActive(true);
+        }
+
+    }
+    
+}
