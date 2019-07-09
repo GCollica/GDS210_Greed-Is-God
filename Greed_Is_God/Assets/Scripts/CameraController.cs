8ac50287df5a24be434134be32f5b2d26b7b8e45
DaraghW <1015125@student.sae.edu.au>
2019-07-10 01:18:12 +0800

    Revert "Revert "Revert "."""
    This reverts commit 8e287d83d63d5b54f06ab894c7b94835094413fd.

--- Greed_Is_God/Assets/Scripts/CameraController.cs
+++ Greed_Is_God/Assets/Scripts/CameraController.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform target;
     public Vector3 offsetPos;
     public float moveSpeed = 5;
     public float smoothSpeed = 0.5f;
-    private GameObject greedGod;
+
     Vector3 targetPos;
-    private GreedGod greedGodScript;
 
-    private void Awake()
-    {
-        greedGodScript = FindObjectOfType<GreedGod>();
-    }
-
-    void LateUpdate()
+    void Update()
     {
         MoveWithTarget();
     }
@@ -24,8 +19,7 @@ public class CameraController : MonoBehaviour
     void MoveWithTarget()
     {
         //Sets the Camera's position to the player + the offset value
-        greedGod = greedGodScript.currentGreedGod.gameObject;
-        targetPos = greedGod.transform.position + offsetPos;
+        targetPos = target.position + offsetPos;
 
         //Sets the cameras target to the player position and smooths the movement  
         transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
