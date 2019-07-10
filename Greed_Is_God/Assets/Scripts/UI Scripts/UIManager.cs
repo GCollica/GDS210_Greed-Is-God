ac09c6f216296e25694c0636cd795685c6aa80d7
GCollica <49055809+GCollica@users.noreply.github.com>
2019-07-10 09:45:35 +0800

    Fixed UI Script

--- Greed_Is_God/Assets/Scripts/UI Scripts/UIManager.cs
+++ Greed_Is_God/Assets/Scripts/UI Scripts/UIManager.cs
@@ -72,179 +72,207 @@ public class UIManager : MonoBehaviour
     public Text p3FinalScore;
     public Text p4FinalScore;
 
-    public static int healthStat;
+    public int p1Health;
+    public int p2Health;
+    public int p3Health;
+    public int p4Health;
 
     GreedGod greedGodScript;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        //Health = GameObject.Find("Player_1").GetComponent<PManager>().health = 3;
-        healthStat = p1PManager.health;
-        //greedGodText = greedGodScript.currentGreedGod;
+        P1ValueUpdate();
+        P2ValueUpdate();
+        P3ValueUpdate();
+        P4ValueUpdate();
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthStat = p1PManager.health;
+        if (p1.activeInHierarchy == true)
+        {
+            P1ValueUpdate();
+        }
 
-        //key
-        p1KeyCountValue = p1PManager.keyCount ;
+        if (p2.activeInHierarchy == true)
+        {
+            P2ValueUpdate();
+        }
+
+        if (p3.activeInHierarchy == true)
+        {
+            P3ValueUpdate();
+        }
+
+        if (p4.activeInHierarchy == true)
+        {
+            P4ValueUpdate();
+        }
+    }
+    
+
+    void P1ValueUpdate()
+    {
+        p1Health = p1PManager.health;
+
+        p1KeyCountValue = p1PManager.keyCount;
         p1KeyCountText.text = "x " + p1KeyCountValue.ToString();
 
-        p2KeyCountValue = p2PManager.keyCount;
-        p2KeyCountText.text = "x " + p2KeyCountValue.ToString();
-
-        p3KeyCountValue = p3PManager.keyCount;
-        p3KeyCountText.text = "x " + p3KeyCountValue.ToString();
-
-        p4KeyCountValue = p4PManager.keyCount;
-        p4KeyCountText.text = "x " + p4KeyCountValue.ToString();
-
-        //gold
         p1GoldCountValue = p1PManager.goldCount;
         p1GoldCountText.text = "$" + p1GoldCountValue.ToString();
         p1FinalScore.text = p1GoldCountValue.ToString();
+
+        if (p1Health == 3)
+        {
+            p1Heart1.gameObject.SetActive(true);
+            p1Heart2.gameObject.SetActive(true);
+            p1Heart3.gameObject.SetActive(true);
+        }
+
+        if (p1Health == 2)
+        {
+            p1Heart1.gameObject.SetActive(true);
+            p1Heart2.gameObject.SetActive(true);
+            p1Heart3.gameObject.SetActive(false);
+        }
+
+        if (p1Health == 1)
+        {
+            p1Heart1.gameObject.SetActive(true);
+            p1Heart2.gameObject.SetActive(false);
+            p1Heart3.gameObject.SetActive(false);
+        }
+
+        if (p1Health == 0)
+        {
+            p1Heart1.gameObject.SetActive(false);
+            p1Heart2.gameObject.SetActive(false);
+            p1Heart3.gameObject.SetActive(false);
+            Debug.Log("P1 Dead");
+        }
+    }
+
+    void P2ValueUpdate()
+    {
+        p2Health = p2PManager.health;
+
+        p2KeyCountValue = p2PManager.keyCount;
+        p2KeyCountText.text = "x " + p2KeyCountValue.ToString();
 
         p2GoldCountValue = p2PManager.goldCount;
         p2GoldCountText.text = "$" + p2GoldCountValue.ToString();
         p2FinalScore.text = p2GoldCountValue.ToString();
 
+        if (p2Health == 3)
+        {
+            p2Heart1.gameObject.SetActive(true);
+            p2Heart2.gameObject.SetActive(true);
+            p2Heart3.gameObject.SetActive(true);
+        }
+
+        if (p2Health == 2)
+        {
+            p2Heart1.gameObject.SetActive(true);
+            p2Heart2.gameObject.SetActive(true);
+            p2Heart3.gameObject.SetActive(false);
+        }
+
+        if (p2Health == 1)
+        {
+            p2Heart1.gameObject.SetActive(true);
+            p2Heart2.gameObject.SetActive(false);
+            p2Heart3.gameObject.SetActive(false);
+        }
+
+        if (p2Health == 0)
+        {
+            p2Heart1.gameObject.SetActive(false);
+            p2Heart2.gameObject.SetActive(false);
+            p2Heart3.gameObject.SetActive(false);
+            Debug.Log("P2 Dead");
+        }
+    }
+
+    void P3ValueUpdate()
+    {
+        p3Health = p3PManager.health;
+
+        p3KeyCountValue = p3PManager.keyCount;
+        p3KeyCountText.text = "x " + p3KeyCountValue.ToString();
+
         p3GoldCountValue = p3PManager.goldCount;
         p3GoldCountText.text = "$" + p3GoldCountValue.ToString();
         p3FinalScore.text = p3GoldCountValue.ToString();
+
+        if (p3Health == 3)
+        {
+            p3Heart1.gameObject.SetActive(true);
+            p3Heart2.gameObject.SetActive(true);
+            p3Heart3.gameObject.SetActive(true);
+        }
+
+        if (p3Health == 2)
+        {
+            p3Heart1.gameObject.SetActive(true);
+            p3Heart2.gameObject.SetActive(true);
+            p3Heart3.gameObject.SetActive(false);
+        }
+
+        if (p3Health == 1)
+        {
+            p3Heart1.gameObject.SetActive(true);
+            p3Heart2.gameObject.SetActive(false);
+            p3Heart3.gameObject.SetActive(false);
+        }
+
+        if (p3Health == 0)
+        {
+            p3Heart1.gameObject.SetActive(false);
+            p3Heart2.gameObject.SetActive(false);
+            p3Heart3.gameObject.SetActive(false);
+            Debug.Log("P3 Dead");
+        }
+    }
+    void P4ValueUpdate()
+    {
+        p4Health = p4PManager.health;
+
+        p4KeyCountValue = p4PManager.keyCount;
+        p4KeyCountText.text = "x " + p4KeyCountValue.ToString();
 
         p4GoldCountValue = p4PManager.goldCount;
         p4GoldCountText.text = "$" + p4GoldCountValue.ToString();
         p4FinalScore.text = p4GoldCountValue.ToString();
 
-        if (this.gameObject.name == "Player_1")
+        if (p4Health == 3)
         {
-            if (healthStat == 3)
-            {
-                p1Heart1.gameObject.SetActive(true);
-                p1Heart2.gameObject.SetActive(true);
-                p1Heart3.gameObject.SetActive(true);
-            }
-
-            if (healthStat == 2)
-            {
-                p1Heart1.gameObject.SetActive(true);
-                p1Heart2.gameObject.SetActive(true);
-                p1Heart3.gameObject.SetActive(false);
-            }
-
-            if (healthStat == 1)
-            {
-                p1Heart1.gameObject.SetActive(true);
-                p1Heart2.gameObject.SetActive(false);
-                p1Heart3.gameObject.SetActive(false);
-            }
-
-            if (healthStat == 0)
-            {
-                p1Heart1.gameObject.SetActive(false);
-                p1Heart2.gameObject.SetActive(false);
-                p1Heart3.gameObject.SetActive(false);
-                Debug.Log("P1 Dead");
-            }
+            p4Heart1.gameObject.SetActive(true);
+            p4Heart2.gameObject.SetActive(true);
+            p4Heart3.gameObject.SetActive(true);
         }
 
-        if (this.gameObject.name == "Player_2")
+        if (p4Health == 2)
         {
-            if (healthStat == 3)
-            {
-                p2Heart1.gameObject.SetActive(true);
-                p2Heart2.gameObject.SetActive(true);
-                p2Heart3.gameObject.SetActive(true);
-            }
-
-            if (healthStat == 2)
-            {
-                p2Heart1.gameObject.SetActive(true);
-                p2Heart2.gameObject.SetActive(true);
-                p2Heart3.gameObject.SetActive(false);
-            }
-
-            if (healthStat == 1)
-            {
-                p2Heart1.gameObject.SetActive(true);
-                p2Heart2.gameObject.SetActive(false);
-                p2Heart3.gameObject.SetActive(false);
-            }
-
-            if (healthStat == 0)
-            {
-                p2Heart1.gameObject.SetActive(false);
-                p2Heart2.gameObject.SetActive(false);
-                p2Heart3.gameObject.SetActive(false);
-                Debug.Log("P2 Dead");
-            }
+            p4Heart1.gameObject.SetActive(true);
+            p4Heart2.gameObject.SetActive(true);
+            p4Heart3.gameObject.SetActive(false);
         }
 
-        if (this.gameObject.name == "Player_3")
+        if (p4Health == 1)
         {
-            if (healthStat == 3)
-            {
-                p3Heart1.gameObject.SetActive(true);
-                p3Heart2.gameObject.SetActive(true);
-                p3Heart3.gameObject.SetActive(true);
-            }
-
-            if (healthStat == 2)
-            {
-                p3Heart1.gameObject.SetActive(true);
-                p3Heart2.gameObject.SetActive(true);
-                p3Heart3.gameObject.SetActive(false);
-            }
-
-            if (healthStat == 1)
-            {
-                p3Heart1.gameObject.SetActive(true);
-                p3Heart2.gameObject.SetActive(false);
-                p3Heart3.gameObject.SetActive(false);
-            }
-
-            if (healthStat == 0)
-            {
-                p3Heart1.gameObject.SetActive(false);
-                p3Heart2.gameObject.SetActive(false);
-                p3Heart3.gameObject.SetActive(false);
-                Debug.Log("P3 Dead");
-            }
+            p4Heart1.gameObject.SetActive(true);
+            p4Heart2.gameObject.SetActive(false);
+            p4Heart3.gameObject.SetActive(false);
         }
 
-        if (this.gameObject.name == "Player_4")
+        if (p4Health == 0)
         {
-            if (healthStat == 3)
-            {
-                p4Heart1.gameObject.SetActive(true);
-                p4Heart2.gameObject.SetActive(true);
-                p4Heart3.gameObject.SetActive(true);
-            }
-
-            if (healthStat == 2)
-            {
-                p4Heart1.gameObject.SetActive(true);
-                p4Heart2.gameObject.SetActive(true);
-                p4Heart3.gameObject.SetActive(false);
-            }
-
-            if (healthStat == 1)
-            {
-                p4Heart1.gameObject.SetActive(true);
-                p4Heart2.gameObject.SetActive(false);
-                p4Heart3.gameObject.SetActive(false);
-            }
-
-            if (healthStat == 0)
-            {
-                p4Heart1.gameObject.SetActive(false);
-                p4Heart2.gameObject.SetActive(false);
-                p4Heart3.gameObject.SetActive(false);
-                Debug.Log("P4 Dead");
-            }
+            p4Heart1.gameObject.SetActive(false);
+            p4Heart2.gameObject.SetActive(false);
+            p4Heart3.gameObject.SetActive(false);
+            Debug.Log("P4 Dead");
         }
-    }   
+    }
 }
